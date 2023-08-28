669e401c683442f8f26fedd7fd5e1509fefb8e7c
nikhil_varavadekar <nikhil_varavadekar@BHL000190.acds.net.in>
2023-08-28 10:06:20 +0000

    init

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      hello world
+    </div>
+  )
+}
+
+export default App
